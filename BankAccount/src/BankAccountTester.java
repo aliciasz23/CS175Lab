dc15da27727b19b6c9d6df193ea0f55aaaecf88f
aliciaortiz <aliciaortiz@10.0.0.40>
2020-09-30 19:15:22 -0400

    Bank account assignment.

---
+++ BankAccount/src/BankAccountTester.java
@@ -0,0 +1,23 @@
+
+
+public class BankAccountTester {
+
+	public static void main(String[] args) {
+		BankAccount myBankAccount = new BankAccount(1000.00);//invokes BankAccount code lines 12-15
+		System.out.println("Bank account beginning balance is: ");
+		System.out.println(myBankAccount.getBalance());
+		
+		myBankAccount.withdraw(500);//runs BankAccount code line 31-34
+		System.out.println("Withdraw: ");
+		System.out.println(myBankAccount.getBalance());
+		
+		myBankAccount.deposit(1500);//runs BankAccount code lines 21-24
+		System.out.println("Deposit: ");
+		System.out.println(myBankAccount.getBalance());
+		
+		System.out.println("Your current balance is: ");
+		System.out.println(myBankAccount.getBalance());//runs BankAccount code lines 39-42
+		
+	}
+
+}
